985a9f109c8dd7ca1ac623de7871a4c8ff005f81
Maksym Kulia <maksym.kulia@protonmail.com>
2020-10-03 23:03:48 +0300

    [add] Description and move to libs

---
+++ libs/basic.js
@@ -0,0 +1,29 @@
+/*
+ * Lib with basics methods
+ */
+
+// Assets
+let p = require("../assets/primary");
+let tc = require("../assets/textColors");
+let bc = require("../assets/backgroundColors");
+
+// Test method
+exports.test = () => {
+  console.log("test");
+};
+
+// Simple console log (e -> echo).
+// Just to keep your code in one format
+exports.e = (content) => {
+  console.log(content, p.reset);
+};
+
+// Console log with colors and styles
+exports.incolor = (content, color) => {
+  console.log(tc[color], content, p.reset);
+};
+
+// Console log with backgound color
+exports.bgcolor = (content, bgcolor) => {
+  console.log(bc[bgcolor], content, p.reset);
+};
